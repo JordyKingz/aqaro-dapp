a973d04d84fb04d7ac85fc2008adc678ab0af364
Jordy <jordy@nefkon.com>
2023-06-12 15:10:11 +0200

    subscribe to newsletter

--- src/components/LaunchNotify.vue
+++ src/components/LaunchNotify.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import Subscribe from "@/components/form/Subscribe.vue";
+
+enum subscribeType {
+    Launch = 'Launch',
+    Newsletter = 'Newsletter'
+}
 </script>
 <template>
     <div class="bg-gray-800 py-16 sm:py-24">
@@ -8,11 +15,11 @@
                 <p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
                     Be the first to experience the future of housing - sign up for our launch notifications and get ready to join the decentralized housing revolution.
                 </p>
-                <form class="mx-auto mt-10 flex max-w-md gap-x-4">
-                    <label for="email-address" class="sr-only">Email address</label>
-                    <input id="email-address" name="email" type="email" autocomplete="email" class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6" placeholder="Enter your email" />
-                    <button type="submit" class="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Notify me</button>
-                </form>
+                <div class="flex max-w-md gap-x-4 mx-auto mt-10 ">
+                    <div class="block">
+                        <Subscribe :subType="subscribeType.Launch" :buttonText="'Notify me'" />
+                    </div>
+                </div>
 
             </div>
         </div>
--- src/lib/http/axios.client.ts
+++ src/lib/http/axios.client.ts
@@ -1,14 +1,38 @@
-import axios from "axios";
+import axios, { type AxiosInstance } from "axios";
+import {useAuthStore} from "@/stores/auth.store";
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_SERVICE_URL,
-});
-
-const axiosAuthInstance = axios.create({
+const axiosInstance: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_SERVICE_URL,
     headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        'Content-Type': 'application/json',
+    },
+});
+
+const axiosAuthInstance: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_SERVICE_URL,
+    headers: {
+        'Content-Type': 'application/json',
     }
 });
 
-export default {axiosInstance, axiosAuthInstance};
+axiosAuthInstance.interceptors.request.use(
+    (config) => {
+        const store = useAuthStore();
+        const token = store.getToken;
+
+        const bearerToken = localStorage.getItem("aqaro.auth.token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        } else if (bearerToken) {
+            config.headers.Authorization = `Bearer ${bearerToken}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export {axiosInstance, axiosAuthInstance};
