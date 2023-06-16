ac0caff4b5a1091abee4bd2849907f46cc3f0c0a
Jordy <jordy@nefkon.com>
2023-06-16 21:59:55 +0200

    copied invest for stake page

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -110,7 +110,7 @@ async function disconnect() {
                     <RouterLink :to="{name: 'about'}" class="text-sm font-semibold leading-6 text-white">About</RouterLink>
 
                     <RouterLink :to="{name: 'early.investor'}" class="text-sm font-semibold leading-6 text-white">Invest</RouterLink>
-<!--                    <RouterLink v-if="Number(userBalance) > 0" :to="{name: 'early.investor'}" class="text-sm font-semibold leading-6 text-white">Stake</RouterLink>-->
+                    <RouterLink v-if="Number(userBalance) > 0" :to="{name: 'early.investor'}" class="text-sm font-semibold leading-6 text-white">Stake</RouterLink>
 
                     <RouterLink :to="{name: 'mortgage.liquidity.provider'}" class="text-sm font-semibold leading-6 text-white">Mortgage Provider</RouterLink>
 
@@ -167,7 +167,7 @@ async function disconnect() {
                             <span v-on:click="routerTo('about')" class="-mx-3 block rounded-md text-gray-300 hover:text-indigo-500 py-2.5 px-3 text-base font-semibold leading-7">About</span>
 
                             <span v-on:click="routerTo('early.investor')" class="-mx-3 block rounded-md text-gray-300 hover:text-indigo-500 py-2.5 px-3 text-base font-semibold leading-7">Invest</span>
-<!--                            <span v-if="Number(userBalance) > 0" v-on:click="routerTo('early.investor')" class="-mx-3 block rounded-md text-gray-300 hover:text-indigo-500 py-2.5 px-3 text-base font-semibold leading-7">Stake</span>-->
+                            <span v-if="Number(userBalance) > 0" v-on:click="routerTo('early.investor')" class="-mx-3 block rounded-md text-gray-300 hover:text-indigo-500 py-2.5 px-3 text-base font-semibold leading-7">Stake</span>
 
                             <span v-on:click="routerTo('mortgage.liquidity.provider')" class="-mx-3 block rounded-md text-gray-300 hover:text-indigo-500 py-2.5 px-3 text-base font-semibold leading-7">Mortgage Provider</span>
 
