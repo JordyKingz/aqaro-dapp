32d853d8cdd12825c322a85560dae70c7e14c4ad
Jordy <jordy@nefkon.com>
2023-07-04 09:51:34 +0200

    vue router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import DaoIndex from '../views/dao/index.vue'
+import NotFoundComponent from '../views/404.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: '/',
@@ -114,7 +115,11 @@ const router = createRouter({
         }
       ],
     },
-
+    {
+      path: '/:catchAll(.*)',
+      component: NotFoundComponent,
+      name: 'NotFound'
+    }
   ]
 })
 
