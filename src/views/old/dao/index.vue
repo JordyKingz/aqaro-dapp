2bce19d3d3a6656d8beb2051562f04a5e4b964cd
Jordy <jordy@nefkon.com>
2023-08-23 10:18:06 +0200

    moved to old components

---
+++ src/views/old/dao/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Sidebar from "@/components/old/layout/dao/Sidebar.vue";
+
+</script>
+<template>
+<div id="dao" class="bg-gray-800">
+    <Sidebar />
+    <RouterView class="bg-gray-800" />
+</div>
+</template>
